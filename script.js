43250f16059c8f75397f8aedcae630c751c3f4e2
jayvardhanghildiyal <jayvghildiyal2016@gmail.com>
2024-05-02 21:05:17 +0530

    added base material required for rps assignment

---
+++ script.js
@@ -0,0 +1,16 @@
+//testing testing testing
+console.log("scissor paper rock game yurp")
+
+function getComputerChoice() {
+    let chance = Math.floor(Math.random() * 3);
+    console.log(chance)
+}
+
+// for testing if the choice function works
+console.log(getComputerChoice())
+
+let linkge;
+switch(choice) {
+    case 0: 
+    linkge
+}
